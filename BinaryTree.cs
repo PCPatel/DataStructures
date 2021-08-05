7e280f4f1d9f69935dc5bad26b808a5082aeea89
PC Patel <patelpc.mits@gmail.com>
2021-08-05 22:30:44 +0530

    -NA-

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -33,12 +33,75 @@ namespace Data_Structures
             return current;
         }
 
+        public void Delete(int value){
+            DeleteNode(root, value);
+        }
+
+        private BTNode DeleteNode(BTNode current, int value){
+            if(current == null){
+                return null;
+            }
+            //search for the value
+            if(value > current.value){
+                current.right = DeleteNode(current.right, value);
+            }
+            else if(value < current.value){
+                current.left = DeleteNode(current.left, value);
+            }
+            else {
+                if(current.left == null && current.right == null){
+                    return null;
+                }
+                else if(current.left == null || current.right == null){
+                    return current.left ?? current.right;
+                }
+                else if(current.left != null && current.right != null){
+                    var successor = current.right;
+                    while(successor.left != null){
+                        successor = successor.left;
+                    }
+                    current.value = successor.value;
+                    current.right = DeleteNode(current.right, successor.value);
+                }
+            }
+            return current;
+        }
+
+        private int i=0;
         public void Print(BTNode current){
             if(current!=null){
+                i = i+4;
+                Print(current.right);//! L
+                Console.WriteLine(new String(' ',i) + current.value);//! N
+                Print(current.left);//! R
+                i = i-4;
+            }
+        }
+
+        // LNR
+        public void Print_LNR(BTNode current){
+            Console.WriteLine("");
+            Console.WriteLine("Print LNR");
+            if(current!=null){
                 Print(current.left);
-                Console.WriteLine(current.value);
+                Console.Write(current.value + " - ");
                 Print(current.right);
             }
         }
+
+        // RNL
+        public void Print_RNL(BTNode current){
+            Console.WriteLine("");
+            Console.WriteLine("Print RNL");
+            if(current!=null){
+                Print(current.right);
+                Console.Write(current.value + " - ");
+                Print(current.left);
+            }
+        }
+
+        // NLR
+        // LNR
+
     }
 }
